4858fe20d9807eb7a185c5521d4071c400af43db
Mikhail Mosyagin <mogbymo@yandex.ru>
2018-09-23 23:22:40 +0300

    Add basic database management
    Run data/main.py to see it in action.

---
+++ database/database.py
@@ -0,0 +1,46 @@
+import logging
+import os
+import sqlite3
+
+
+class PeerReviewDB:
+    logger = logging.getLogger('database')
+
+    def __init__(self, db_file):
+        self.db_file = db_file
+        if not os.path.exists(db_file):
+            self._create_db()
+
+    def _create_db(self):
+        PeerReviewDB.logger.info(
+            'Creating database at {}'.format(os.path.abspath(self.db_file))
+        )
+
+        with self._create_connection() as db:
+            db.execute('''
+            CREATE TABLE user (
+                login text PRIMARY KEY
+            )''')
+            db.execute('''
+            CREATE TABLE task (
+                name        text PRIMARY KEY,
+                timestamp   int
+            )''')
+            db.execute('''
+            CREATE TABLE user_task (
+                id   int    PRIMARY KEY,
+                user text   REFERENCES user(login),
+                task text   REFERENCES task(name),
+                url  text
+            )''')
+            db.execute('''
+            CREATE TABLE review (
+               reviewer text    REFERENCES user(login),
+               task_id  int     REFERENCES user_task(id),
+               status   int
+            )''')
+
+    def _create_connection(self):
+        db = sqlite3.connect(self.db_file)
+        db.execute('PRAGMA foreign_keys = 1')
+        return db
---
+++ database/main.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+
+from database import PeerReviewDB
+
+import configparser
+import logging
+import logging.config
+import sys
+
+
+def configure_logger(log_file=None):
+    logger = PeerReviewDB.logger
+    logger.setLevel(logging.DEBUG)
+
+    formatter = logging.Formatter(
+        '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+    )
+
+    console_handler = logging.StreamHandler()
+    console_handler.setLevel(logging.INFO)
+    console_handler.setFormatter(formatter)
+    logger.addHandler(console_handler)
+
+    if log_file is not None:
+        file_handler = logging.FileHandler(log_file)
+        file_handler.setLevel(logging.DEBUG)
+        file_handler.setFormatter(formatter)
+        logger.addHandler(file_handler)
+
+
+def main():
+    config_file = sys.argv[1]
+    config = configparser.ConfigParser()
+    config.read(sys.argv[1])
+
+    configure_logger(config.get('database', 'log_file', fallback=None))
+
+    db = PeerReviewDB(config['database']['db_file'])
+    db.create_db()
+
+
+if __name__ == '__main__':
+    main()
