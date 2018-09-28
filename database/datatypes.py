c2a1dcc23b574618143f7a9a63408037c8ccb16f
mosyagin-m <mosyagin-m@mosyagin-m-nix.yandex.net>
2018-09-28 17:05:55 +0300

    Update schema
     - Add constraints and relationships
     - IN_PROGRESS -> IN_WORK in status enums

--- database/datatypes.py
+++ database/datatypes.py
@@ -2,19 +2,20 @@ import enum
 
 from sqlalchemy import Column
 from sqlalchemy import Enum, Integer, String, Time
-from sqlalchemy import ForeignKey
+from sqlalchemy import ForeignKey, UniqueConstraint
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
 
 
 class TaskStatus(enum.Enum):
     WAITING_REVIEW  = 1
-    IN_PROGESS      = 2
+    IN_WORK         = 2
     COMPLETE        = 3
 
 
 class ReviewStatus(enum.Enum):
     WAITING_REVIEWER    = 1
-    IN_PROGRESS         = 2
+    IN_WORK             = 2
     CLOSED              = 3
 
 
@@ -26,6 +27,8 @@ class User(Base):
 
     id      = Column(Integer, primary_key=True)
     login   = Column(String(40))
+    
+    UniqueConstraint(login)
 
 
 class Task(Base):
@@ -33,6 +36,8 @@ class Task(Base):
 
     id      = Column(Integer, primary_key=True)
     name    = Column(String(100), nullable=False)
+
+    UniqueConstraint(name)
 
 
 class UserTask(Base):
@@ -45,12 +50,31 @@ class UserTask(Base):
     status      = Column(Enum(TaskStatus), default=TaskStatus.WAITING_REVIEW, nullable=False)
     timestamp   = Column(Time, nullable=False)
 
+    UniqueConstraint(user_id, task_id)
+
+    user = relationship(User, back_populates='tasks')
+    task = relationship(Task, back_populates='attempts')
+
+
+User.tasks    = relationship(UserTask, order_by=UserTask.id, back_populates='user')
+Task.attempts = relationship(UserTask, order_by=UserTask.id, back_populates='task')
+
 
 class Review(Base):
     __tablename__ = 'review'
 
-    id          = Column(Integer, primary_key=True)
-    reviewer_id = Column(Integer, ForeignKey('user.id'), nullable=False)
-    task_id     = Column(Integer, ForeignKey('user_task.id'), nullable=False)
-    status      = Column(Time, nullable=False)
+    id                  = Column(Integer, primary_key=True)
+    reviewer_id         = Column(Integer, ForeignKey('user.id'), nullable=False)
+    reviewed_task_id    = Column(Integer, ForeignKey('user_tasks.id'), nullable=False)
+    status              = Column(Time, nullable=False)
+    issue_url           = Column(String(200))
+
+    UniqueConstraint(issue_url)
+
+    reviewer        = relationship(User, back_populates='outgoing_reviews')
+    reviewed_task   = relationship(UserTask, back_populates='reviews')
+
+
+User.outgoing_reviews = relationship(Review, order_by=Review.id, back_populates='reviewer')
+UserTask.reviews      = relationship(Review, order_by=Review.id, back_populates='reviewed_task')
 
