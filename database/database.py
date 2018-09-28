4ea25361fb221479b510678c54773346f52e6f38
mosyagin-m <mosyagin-m@mosyagin-m-nix.yandex.net>
2018-09-28 17:08:46 +0300

    Move PeerReviewDB to SQLAlchemy

--- database/database.py
+++ database/database.py
@@ -1,198 +1,50 @@
 import logging
-import os.path
-import sqlite3
 
+from contextlib import contextmanager
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from datatypes import TaskStatus, ReviewStatus
 from datatypes import User, Task, UserTask, Review
 
 
-def _convert_list_decorator(to_type):
-    def decorator_impl(func):
-        def wrapper(*args, **kwargs):
-            result = func(*args, **kwargs)
-            return list(map(lambda v: to_type(*v), result))
-        return wrapper
-    return decorator_impl
-
-
-def _convert_decorator(to_type):
-    def decorator_impl(func):
-        def wrapper(*args, **kwargs):
-            result = func(*args, **kwargs)
-            if result is not None:
-                return to_type(*result)
-        return wrapper
-    return decorator_impl
-
-
-# TODO: user an orm library
 class PeerReviewDB:
-    logger = logging.getLogger('database')
+    def __init__(self, db_url, override_db=False):
+        self._engine = create_engine(db_url, echo=True)
+        self._session_factory = sessionmaker(bind=engine)
 
-    def __init__(self, db_file, override_db=False):
-        self.db_file = db_file
-        if not os.path.exists(db_file):
-            PeerReviewDB.logger.info(
-                'Creating database at {}'.format(os.path.abspath(self.db_file))
-            )
-            self._create_db()
-        elif override_db:
-            PeerReviewDB.logger.info(
-                'Overriding database at {}'.format(os.path.abspath(self.db_file))
-            )
-            os.remove(db_file)
-            self._create_db()
-        else:
-            PeerReviewDB.logger.info(
-                'Using database found at {}'.format(os.path.abspath(self.db_file))
-            )
+    @contextmanager
+    def start_session(self):
+        self._session = self._session_factory()
+        yield None
+        self._session.close()
 
-    @_convert_decorator(User)
     def get_user(self, login):
-        with self._create_connection() as db:
-            return db.execute(
-                'SELECT * FROM user WHERE login = ?',
-                (login,)
-            ).fetchone()
+        return self._session.query(User).filter(User.login == login).first()
 
-    @_convert_decorator(Task)
     def get_task(self, name):
-        with self._create_connection() as db:
-            return db.execute(
-                'SELECT * FROM task WHERE name = ?',
-                (name,)
-            ).fetchone()
+        return self._session.query(Task).filter(Task.name == name).first()
 
-    @_convert_decorator(UserTask)
     def get_user_task(self, user, task):
-        with self._create_connection() as db:
-            return db.execute('''
-                SELECT * FROM user_task
-                WHERE user = ? AND task = ?
-                ''',
-                (user, task)
-            ).fetchone()
+        return self._session.query(UserTask).filter(
+            UserTask.user_id == user.id,
+            UserTask.task_id == task.id,
+        ).first()
 
-    @_convert_decorator(Review)
-    def get_review(self, reviewer, reviewee, task):
-        with self._create_connection() as db:
-            return db.execute('''
-                SELECT * FROM review
-                WHERE reviewer = ? AND reviewee = ? AND task = ?
-                ''',
-                (reviewer, reviewee, task)
-            ).fetchone()
+    def get_review(self, reviewer, user_task):
+        return self._session.query(Review).filter(
+            Review.reviewer_id  == reviewer.id,
+            Review.user_task_id == user_task.id,
+            Review.status       != ReviewStatus.CLOSED,
+        ).first()
 
-    @_convert_list_decorator(User)
-    def get_all_users(self):
-        with self._create_connection() as db:
-            return db.execute('SELECT * FROM user').fetchall()
+    def get_all_records(record_type):
+        return self._session.query(record_type).all()
 
-    @_convert_list_decorator(Task)
-    def get_all_tasks(self):
-        with self._create_connection() as db:
-            return db.execute('SELECT * FROM task').fetchall()
+    def add_record(self, record):
+        self._session.add(record)
 
-    @_convert_list_decorator(UserTask)
-    def get_all_user_tasks(self):
-        with self._create_connection() as db:
-            return db.execute('SELECT * FROM user_task').fetchall()
-
-    @_convert_list_decorator(Review)
-    def get_all_reviews(self):
-        with self._create_connection() as db:
-            return db.execute('SELECT * FROM review').fetchall()
-
-    def add_user(self, user):
-        with self._create_connection() as db:
-            db.execute('INSERT INTO user VALUES (?)', (user.login,))
-
-    def add_task(self, task):
-        with self._create_connection() as db:
-            db.execute('INSERT INTO task VALUES (?)', (task.name,))
-
-    def add_user_task(self, user_task):
-        with self._create_connection() as db:
-            db.execute(
-                'INSERT INTO user_task VALUES (?, ?, ?, ?, ?)',
-                (
-                    user_task.user,
-                    user_task.task,
-                    user_task.url,
-                    user_task.status,
-                    user_task.timestamp
-                )
-            )
-
-    def add_review(self, review):
-        with self._create_connection() as db:
-            db.execute(
-                'INSERT INTO review VALUES (?, ?, ?, ?)',
-                (
-                    review.reviewer,
-                    review.reviewee,
-                    review.task,
-                    review.status
-                )
-            )
-
-    def remove_user(self, login):
-        with self._create_connection() as db:
-            db.execute('DELETE FROM user WHERE login = ?', (login,))
-
-    def remove_task(self, name):
-        with self._create_connection() as db:
-            db.execute('DELETE FROM task WHERE name = ?', (name,))
-
-    def remove_user_task(self, user, task):
-        with self._create_connection() as db:
-            db.execute('''
-                DELETE FROM user_task
-                WHERE user = ? AND task = ?
-                ''',
-                (user, task)
-            )
-
-    def remove_review(self, reviewer, reviewee, task):
-        with self._create_connection() as db:
-            db.execute('''
-                DELETE FROM review
-                WHERE reviewer = ? AND reviewee = ? AND task = ?
-                ''', 
-                (reviewer, reviewee, task)
-            )
-
-    def _create_db(self):
-        with self._create_connection() as db:
-            db.execute('''
-            CREATE TABLE user (
-                login text PRIMARY KEY
-            )''')
-            db.execute('''
-            CREATE TABLE task (
-                name text PRIMARY KEY
-            )''')
-            db.execute('''
-            CREATE TABLE user_task (
-                user        text    REFERENCES user(login),
-                task        text    REFERENCES task(name),
-                url         text,
-                status      int,
-                timestamp   int,
-                PRIMARY KEY (user, task)
-            )''')
-            db.execute('''
-            CREATE TABLE review (
-                reviewer text    REFERENCES user(login),
-                reviewee text,
-                task     text,
-                status   int,
-                PRIMARY KEY (reviewer, reviewee, task)
-                FOREIGN KEY (reviewee, task) REFERENCES user_task(user, task)
-            )''')
-
-    def _create_connection(self):
-        db = sqlite3.connect(self.db_file)
-        db.set_trace_callback(PeerReviewDB.logger.debug)
-        db.execute('PRAGMA foreign_keys = 1')
-        return db
+    def delete_record(self, record):
+        self._session.delete(record)
 
